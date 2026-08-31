08ff9fbfc757f1edabc475a9d2562fc1d3fd7ac3
JhenyferFrigeri-dotcom <jhenyfer.frigeri@escola.pr.gov.br>
2026-08-31 18:05:34 -0300

    .

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Seleciona o botão de curtir especificamente pelo ID
 const botaoCurtir = document.getElementById("botao-curtir");
 
 botaoCurtir.addEventListener("click", function() {
@@ -5,6 +6,7 @@ botaoCurtir.addEventListener("click", function() {
     texto.textContent++;
 });
 
+// Evento do botão de modo escuro
 document.getElementById("botao-escuro").addEventListener("click", function() {
     document.body.classList.toggle("modo-escuro");
 });
