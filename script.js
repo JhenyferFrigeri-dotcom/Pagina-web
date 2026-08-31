6a3055f1ae298eedac3b764183771ff298958c47
JhenyferFrigeri-dotcom <jhenyfer.frigeri@escola.pr.gov.br>
2026-08-31 17:34:22 -0300

    .

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
- const botao = document.querySelector("button");
-        botao.addEventListener("click", botaoClicado);
+ // sleciiona o botão de curtir especificamente pelo ID
+const botaoCurtir = document.getElementById("botao-curtir");
 
-        function botaoClicado() {
-            console.log("Fui clicado");
-            let texto = botao.querySelector("span");
-            texto.textContent++;
-        }
-        document.getElementById("botao-escuro").addEventListener("click", function() {
+botaoCurtir.addEventListener("click", function() {
+    let texto = botaoCurtir.querySelector("span");
+    texto.textContent++;
+});
+
+//botão de modo escuro
+document.getElementById("botao-escuro").addEventListener("click", function() {
     document.body.classList.toggle("modo-escuro");
 });
