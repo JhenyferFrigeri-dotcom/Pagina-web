74b1808f4e93845513d5e8671ae134c1081466f3
JhenyferFrigeri-dotcom <jhenyfer.frigeri@escola.pr.gov.br>
2026-08-31 21:15:49 -0300

    ; )

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-// seleciiona o botão de curtir
+// seleciiona o botao de curtir
 const botaoCurtir = document.getElementById("botao-curtir");
+
+// variavel para controlar se o botao ja foi clicado (inicia como falso)
+let curtiu = false;
 
 botaoCurtir.addEventListener("click", function() {
     let texto = botaoCurtir.querySelector("span");
-    texto.textContent++;
+
+    // se ainda nao curtiu soma +1 e muda a variavel para true
+    if (curtiu === false) {
+        texto.textContent++;
+        curtiu = true;
+    } 
+    // se ja tinha curtido e clicou de novo subtrai -1 e volta para false
+    else {
+        texto.textContent--;
+        curtiu = false;
+    }
 });
 
-//botão de modo escuro
+// Botao de modo escuro
 document.getElementById("botao-escuro").addEventListener("click", function() {
     document.body.classList.toggle("modo-escuro");
 });
